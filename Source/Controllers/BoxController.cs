e8d10ee0fd809922b124f5327d1a346720c37492
Kajetan Jóźwiak <unvios22@gmail.com>
2024-07-08 20:08:11 +0200

    Setup directory layout

---
+++ Source/Controllers/BoxController.cs
@@ -0,0 +1,11 @@
+using Godot;
+using System;
+
+public partial class BoxController : Node {
+	public override void _Ready() {
+	}
+	
+	public override void _Process(double delta) {
+		GD.Print("mlem");
+	}
+}
