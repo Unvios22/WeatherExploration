dd39aca457643f473a380e950387bddf20b33c8f
Kajetan Jóźwiak <unvios22@gmail.com>
2024-07-10 03:16:17 +0200

    Add camera InputActions

---
+++ Source/Input/InputActions.cs
@@ -0,0 +1,7 @@
+﻿namespace WeatherExploration.Source.Input;
+
+public static class InputActions {
+    public const string CAMERA_DRAG = "CameraDrag";
+    public const string CAMERA_ZOOM_IN = "CameraZoomIn";
+    public const string CAMERA_ZOOM_OUT = "CameraZoomOut";
+}
