c876ae29c9491764b90092473802a0e5580a76c3
Kajetan Jóźwiak <unvios22@gmail.com>
2024-08-05 19:09:02 +0200

    Add Autoload RayCasterNode3D helper class

---
+++ Source/Autoload/RayCasterNode3D.cs
@@ -0,0 +1,66 @@
+﻿using Godot;
+
+namespace WeatherExploration.Source.Autoload;
+
+public partial class RayCasterNode3D : Node3D {
+    //helper class to get custom raycasts from code
+    //the API exposes direct calls, but these are wholly inefficient, see
+    //https://sampruden.github.io/posts/godot-is-not-the-new-unity/#godot
+    //as of 05.09.24 the API hasn't been changed still: (4.2 docs)
+    //https://docs.godotengine.org/en/stable/tutorials/physics/ray-casting.html
+    
+    //the class instantiates an autoload RaycastNode3D and then dynamically changes its world pos to query the raycasts
+
+    private RayCast3D _rayCast3DNode;
+
+    public bool RayCastInDirection(Vector3 origin, Vector3 direction, out HitInfo hitInfo) {
+        var target = origin + direction;
+        return RayCastInternal(origin, target, out hitInfo);
+    }
+
+    public bool RayCastToTarget(Vector3 origin, Vector3 target, out HitInfo hitInfo) {
+        return RayCastInternal(origin, target, out hitInfo);
+    }
+    
+    private bool RayCastInternal(Vector3 origin, Vector3 target, out HitInfo hitInfo) {
+        _rayCast3DNode.Position = origin;
+        _rayCast3DNode.TargetPosition = target;
+        _rayCast3DNode.ForceRaycastUpdate();
+
+        var isCollisionFound = _rayCast3DNode.IsColliding();
+
+        if (isCollisionFound) {
+            var collisionPoint = _rayCast3DNode.GetCollisionPoint();
+            var collisionDistance = (collisionPoint - origin).Length();
+        
+            hitInfo = new HitInfo() {
+                CollisionPoint = collisionPoint,
+                CollisionDistance = collisionDistance,
+                CollisionNormal = _rayCast3DNode.GetCollisionNormal(),
+                CollisionObject = _rayCast3DNode.GetCollider()
+            };
+        }
+        else {
+            hitInfo = new HitInfo();
+        }
+        
+        return isCollisionFound;
+    }
+    
+    public override void _EnterTree() {
+        InstantiateRayCastNodeAsChild();
+    }
+
+    private void InstantiateRayCastNodeAsChild() {
+        _rayCast3DNode = new RayCast3D();
+        _rayCast3DNode.Name = "RayCast3D";
+        AddChild(_rayCast3DNode);
+    }
+}
+
+public record struct HitInfo {
+    public Vector3 CollisionPoint;
+    public float CollisionDistance;
+    public Vector3 CollisionNormal;
+    public GodotObject CollisionObject;
+}
--- Source/Helper/GetAutoload.cs
+++ Source/Helper/GetAutoload.cs
@@ -1,5 +1,4 @@
-﻿using Godot;
-using WeatherExploration.Source.Autoload;
+﻿using WeatherExploration.Source.Autoload;
 
 namespace WeatherExploration.Source.Helper;
 
@@ -12,14 +11,30 @@ public static class GetAutoload {
     //one could also write an extension method for Godot.Node -> but then still you need an instance to use the ".GetGlobal()" on.
     //also - since Global is a node itself, the code suggestion quickly ends up proposing .GetGlobal().GetGlobal().GetGlobal().
     //one other workaround (than the static Get class) could be just using a custom Node class inheriting Godot.Node for custom stuff like this?
-    
+
+    private static Global _globalCache;
+    private static RayCasterNode3D _rayCasterNode3DCache;
     
     //Method to get Global class that's set up to be Autoloaded in project settings
     //It should always be directly under root; any instantiated scenes being siblings
     public static Global Global() {
+        if (_globalCache is not null) {
+            return _globalCache;
+        }
+        
         //getting NodeTree root as Node to use GetNode there and allow Get.Global() usage in a static context
         var root = Utils.GetNodeTreeRoot();
-        var global = (Global) root.GetNode("Global");
-        return global;
+        _globalCache = (Global) root.GetNode("Global");
+        return _globalCache;
+    }
+
+    public static RayCasterNode3D RayCasterNode3D() {
+        if (_rayCasterNode3DCache is not null) {
+            return _rayCasterNode3DCache;
+        }
+        
+        var root = Utils.GetNodeTreeRoot();
+        _rayCasterNode3DCache = (RayCasterNode3D) root.GetNode("RayCasterNode3D");
+        return _rayCasterNode3DCache;
     }
 }
